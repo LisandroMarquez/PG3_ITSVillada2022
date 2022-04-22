78269fe430814f2cb9349e971cc617a5ae914382
Lichi <lichimarquez04@gmail.com>
2022-04-22 12:07:32 -0300

    Ejercicio 2 resuelto

--- Ejercicios_POO_python/Ejercicio 2.py
+++ Ejercicios_POO_python/Ejercicio 2.py
@@ -1,19 +1,19 @@
 # Inicializar la clase 
 class Alumno:
     def __init__(self, nombre: str, nota: int) -> None:
-        self.nombre = nombre
-        self.nota = nota
+        self.nombre: str = nombre
+        self.nota: int = nota
     def __str__(self) -> str:
-        return "Mi nombre es "+self.nombre+", mi nota es "+str(self.nota)
+        return f"Mi nombre es {self.nombre}, mi nota es {str(self.nota)}"
     def Aprobado(self) -> str:
-        if self.nota>=7:
+        if self.nota >= 7:
             return "Y estoy aprobado"
         else:
             return "Y estoy desaprobado"
 
 # Instanciar objetos
-alumno1=Alumno("Pepe",4)
-alumno2=Alumno("Lichi", 9)
+alumno1 = Alumno("Pepe",4)
+alumno2 = Alumno("Lichi", 9)
 
 # Imprimir objetos
 print(alumno1.__str__())
