b544900b3e8d6cab4805e7cb7a3b5a5484b75436
Lichi <lichimarquez04@gmail.com>
2022-04-22 11:08:53 -0300

    Ejercicio 3 resuelto

--- Ejercicios_POO_python/Ejercicio 3.py
+++ Ejercicios_POO_python/Ejercicio 3.py
@@ -4,4 +4,25 @@ class Triangulo:
         self.lado1 = l1
         self.lado2 = l2
         self.lado3 = l3
-    
+    def ladoMayor(self) -> str:
+        if self.lado1 > self.lado2 and self.lado1 > self.lado3:
+            return "El lado mayor es "+str(self.lado1)
+        elif self.lado2 > self.lado1 and self.lado2 > self.lado3:
+            return "El lado mayor es "+str(self.lado2)
+        else:
+            return "El lado mayor es "+str(self.lado3)
+    def Equilatero(self) -> str:
+        if self.lado1 == self.lado2 and self.lado1 == self.lado3:
+            return "El triangulo es equilatero"
+        else:
+            return "El triangulo no es equilatero"
+
+# Instanciar objetos
+triangulo1 = Triangulo(3, 3, 3)
+triangulo2 = Triangulo(3, 5, 4)
+
+# Imprimir objetos
+print(triangulo1.ladoMayor())
+print(triangulo1.Equilatero())
+print(triangulo2.ladoMayor())
+print(triangulo2.Equilatero())
