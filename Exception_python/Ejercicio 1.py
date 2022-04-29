04cc175dea23c0d6b2f178e3e38236079e113a19
Lichi <lichimarquez04@gmail.com>
2022-04-29 12:01:40 -0300

    Ejercicio 1 finalizado

--- Exception_python/Ejercicio 1.py
+++ Exception_python/Ejercicio 1.py
@@ -1,8 +1,11 @@
 flag: bool = True
+numero1: int = 0
+numero2: int = 0
+x: int = 0
 while flag:
     try:
-        numero1: int = int(input("Ingrese el primer numero: "))
-        numero2: int = int(input("Ingrese el segundo numero: "))
+        numero1 = int(input("Ingrese el primer numero: "))
+        numero2 = int(input("Ingrese el segundo numero: "))
         print(f"La suma entre {numero1} y {numero2} es: {numero1 + numero2}")
     except ValueError as exc:
         print(f"Error! No se puede dividir con caracteres {exc.args}")
