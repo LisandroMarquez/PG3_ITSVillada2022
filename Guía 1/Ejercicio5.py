ded42f4e323b15c6706cece2b9d42d31cb39749f
Lichi <lichimarquez04@gmail.com>
2022-04-29 08:56:10 -0300

    Ejercicio 5 corregido

--- Guía 1/Ejercicio5.py
+++ Guía 1/Ejercicio5.py
@@ -5,11 +5,11 @@ def Palindromo(a: str) -> bool:
     return a == a[::-1] # Verifica que el caracter 0 sea igual al -1(el último) y así sucesivamente
  
 # Ingresar la palabra
-palabra = input("Ingrese una palabra para determinar si es un palíndromo:")
+palabra = input("Ingrese una palabra para determinar si es un palíndromo: ")
 
 # Comprobar si es un palíndromo
 flag = Palindromo(palabra)
 if flag == True:
-    print('La palabra "'+palabra+'" si es un palíndromo')
+    print(f"La palabra {palabra} si es un palíndromo")
 else:
-    print('La palabra "'+palabra+'" no es un palíndromo')
+    print(f"La palabra {palabra} NO es un palíndromo")
