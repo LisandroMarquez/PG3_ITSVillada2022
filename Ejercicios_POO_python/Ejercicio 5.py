e6a543c0048d4a13e88cfd210cf990fcd40172f7
Lichi <lichimarquez04@gmail.com>
2022-04-29 09:07:46 -0300

    Ejercicio 5 con correcciones

--- Ejercicios_POO_python/Ejercicio 5.py
+++ Ejercicios_POO_python/Ejercicio 5.py
@@ -3,7 +3,7 @@ class Persona:
     def __init__(self, n: str, e: int) -> None:
         self.nombre: str = n
         self.edad: int = e
-    def imprimir(self) -> None:
+    def __str__(self) -> None:
         return f"Nombre: {self.nombre}\nEdad: {self.edad}"
 
 # Instanciar objetos
@@ -11,16 +11,16 @@ p1 = Persona("Juan", 36)
 p2 = Persona("Pedro", 25)
 
 # Imprimir objetos
-print(p1.imprimir())
-print(p2.imprimir())
+print(p1)
+print(p2)
 
 # Inicializar la clase
 class Empleado(Persona):
     def __init__(self, a: str, b: int) -> None:
         super().__init__(a, b)
         self.sueldo: float = float(input(f"Ingrese el sueldo de {self.nombre}: "))
-    def imprimir(self) -> None:
-        return f"{super().imprimir()} \nSueldo: {self.sueldo}\n{self.impuestos()}"
+    def __str__(self) -> None:
+        return f"{super().__str__()} \nSueldo: {self.sueldo}\n{self.impuestos()}"
     def impuestos(self) -> None:
         if self.sueldo > 3000:
             return "El empleado debe pagar impuestos"
@@ -32,5 +32,5 @@ e1 = Empleado("Lolo", 19)
 e2 = Empleado("Mateo", 46)
 
 # Imprimir objetos
-print(e1.imprimir())
-print(e2.imprimir())
+print(e1)
+print(e2)
