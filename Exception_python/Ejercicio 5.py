2bf3557e4c61b780c6ba8545993c17a41f9dee78
Lichi <lichimarquez04@gmail.com>
2022-05-06 08:36:50 -0300

    Ejercicio 5 finalizado

--- Exception_python/Ejercicio 5.py
+++ Exception_python/Ejercicio 5.py
@@ -4,5 +4,7 @@ try:
     var.close()
 except IOError as exc:
     print(f"Error! No se pudo abrir el archivo. {exc}")
+except TypeError as exc:
+    print(f"Error! El tipo de dato ingresado no es correcto. {exc}")
 except Exception as exc:
     print(f"Error! Error desconocido. {exc}")
